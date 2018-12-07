96f25ae5f7c67701ddcac016788a0cb4b434f713
Anthony Quivers <anthonyquivers@gmail.com>
2018-12-07 12:02:34 -0500

    GET methods

--- fsServer.js
+++ fsServer.js
@@ -19,3 +19,43 @@ http.createServer(function(request, response) {
         respond(405, "Method " + request.method + " not allowed");
 }).listen(8000);
 console.log("File Server is in 8000")
+
+function urlToPath(url) {
+    var path = require("url").parse(url).pathname;
+    return "." + decodeURIComponent(path);
+}
+
+methods.GET = function(path, respond) {
+    fs.stat(path, function(error, stats) {
+        if (error && error.code == "ENOENT")
+            respond(404, "File not found");
+        else if (error)
+            respond(500, error.toString());
+        else if (stats.isDirectory())
+            fs.readdir(path, function(error, files) {
+                if (error)
+                    respond(500, error.toString());
+                else
+                    respond(200, files.join("\n"));
+            });
+        else
+            respond(200, fs.createReadStream(path), require("mime").lookup(path));
+    });
+};
+
+methods.DELETE = function(path, respond) {
+
+};
+
+function respondErrorOrNothing(respond) {
+    return function(error) {
+        if (error)
+            respond(500, error.toString());
+        else
+            respond(204);
+    };
+}
+
+methods.PUT = function(path, respond, request) {
+
+};
